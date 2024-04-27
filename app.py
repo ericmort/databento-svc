540c70173b7d1feaee4aa8082051a66eaf5a209d
Eric Mortensen <ericmorte@gmail.com>
2024-04-27 13:34:46 +0200

    fix debug

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ livedata.setup_livedata(app)
 
 @app.route('/es')
 def get_es():
+    app.logger.info("received request")
+        
     for x in livedata.data:
         app.logger.debug({
             "instrument_id" : x["instrument_id"]
         })
-        
+    app.logger.info(livedata.data)
     return livedata.data
 
 if __name__ == '__main__':
