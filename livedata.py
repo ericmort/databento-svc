ab58734f313f6e4ffe7235b5715276071e764a18
Eric Mortensen <ericmorte@gmail.com>
2024-04-27 19:40:49 +0200

    more fixes

--- livedata.py
+++ livedata.py
@@ -9,6 +9,9 @@ def setup_livedata(app):
     if key is None:
         raise Exception ("Unknown DATABENTO_API_KEY")
     
+    masked = len(key[:-4])*"#"+key[-4:]
+    app.logger.debug(f'DATABENTO_API_KEY: ${masked}')
+
     # Create a live client
     client = db.Live(key=key)
 
@@ -22,14 +25,13 @@ def setup_livedata(app):
     )
 
     def user_callback(record: db.DBNRecord) -> None:
-        #print(f"callback run for {record}")
-        app.logger.debug(record)
+        app.logger.info(f"callback: ${record}")
         data.append({"instrument_id" : record.instrument_id})
 
 
     # Create a callback to handle exceptions from `user_callback`
     def error_handler(exception: Exception) -> None:
-        print(f"an error occurred {exception}")
+        app.logger.info(f"an error occurred {exception}")
 
     client.add_callback(
         record_callback=user_callback,
@@ -37,4 +39,4 @@ def setup_livedata(app):
     )
 
     client.start()
-
+    app.logger.info("Subscription started.")
